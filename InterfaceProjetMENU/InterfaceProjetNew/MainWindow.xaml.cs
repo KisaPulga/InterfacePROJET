b98e76d269151d77e47388353dfdc55c05e6c9cb
KisaPulga <leoschaeffer03@gmail.com>
2025-02-11 17:21:42 +0100

    Debut Interfaces

---
+++ InterfaceProjetMENU/InterfaceProjetNew/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace InterfaceProjetNew
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
